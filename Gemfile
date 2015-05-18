5cb15ea130d5f49da1607de4e9e1b0892d6a0147
Juan Carlos <zalapeach@gmail.com>
2015-05-18 13:56:55 -0500

    Adds gems and environment files

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ ruby '2.1.2'
 
 gem 'figaro', '1.1.1'
 gem 'haml-rails'
+gem 'jquery-rails'
 gem 'pg'
 gem 'rails', '4.1.1'
+gem 'sass-rails', '~> 4.0.0'
 
 gem 'rails_12factor', group: :production
---
+++ config/environments/test.rb
@@ -0,0 +1,20 @@
+Zalapeach::Application.configure do
+  config.cache_classes = true
+
+  config.eager_load = false
+
+  config.serve_static_assets = true
+  config.static_cache_control = 'public, max-age=3600'
+
+  config.consider_all_request_local = true
+  config.action_controller.perform_catching = false
+
+  config.action_dispatch.show_exeptions = false
+
+  config.action_controller.allow_forgery_protection = false
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.active_support.deprecation = :stderr
+end
