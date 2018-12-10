4d54fc91c1fa1f690529a462f161972919015d90
Juan Carlos <jcaguila@mx1.ibm.com>
2018-12-10 16:22:32 -0600

    follow new conventions for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   def self.omniauth_auth(omniauth_info)
     data = omniauth_info.info
     user = User.where(email: data[:email]).first
